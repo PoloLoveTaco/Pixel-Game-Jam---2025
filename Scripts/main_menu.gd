65b0d7e4f174a59048a96c03ef644e7fc543852f
Polo <jules.trolle@epitech.eu>
2025-05-12 03:49:01 +0800

    [ADD] Nav in main menu

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -3,3 +3,15 @@ extends CanvasLayer
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_continue_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/first_act.tscn")
+
+
+func _on_new_game_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/first_act.tscn")
+
+
+func _on_option_button_pressed() -> void:
+	pass # Replace with function body.
