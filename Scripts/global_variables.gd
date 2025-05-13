762970dd1076ebf89ec6c6660015a707c6c567df
Polo <jules.trolle@epitech.eu>
2025-05-13 15:41:52 +0800

    [ADD] global variable in the game

--- Scripts/global_variables.gd
+++ Scripts/global_variables.gd
@@ -1,3 +1,7 @@
 extends Node
 
-@onready var back_white_amout: float = 0.00
+@export_range(0, 1, 0.01)
+var bw_amount := 0.0 :
+	set(value):
+		bw_amount = clamp(value, 0.0, 1.0)
+		RenderingServer.global_shader_parameter_set("bw_amount_global", bw_amount)
