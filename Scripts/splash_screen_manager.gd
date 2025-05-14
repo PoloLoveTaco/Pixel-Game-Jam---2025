e7804c06b6bd59cf6fdf79e51a73beafe5c61c36
Adrien Picot <picotadrien20@gmail.com>
2025-05-14 11:29:58 +0800

    [ADD] can skip splash

--- Scripts/splash_screen_manager.gd
+++ Scripts/splash_screen_manager.gd
@@ -6,3 +6,7 @@ func _ready() -> void:
 	animation_player.play("spawn")
 	await animation_player.animation_finished
 	get_tree().change_scene_to_file("res://Scenes/Menus/main_menu.tscn")
+	
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		get_tree().change_scene_to_file("res://Scenes/Menus/main_menu.tscn")
