a9df01d9089ede26d67ca4c3c36749ddf16a12a4
Polo <jules.trolle@epitech.eu>
2025-05-17 14:04:21 +0800

    [ADD] collider with kitchen, wall in upstairs and shower room + add caca anim

--- Scripts/lobby_house_upstairs.gd
+++ Scripts/lobby_house_upstairs.gd
@@ -3,12 +3,15 @@ extends Node2D
 @onready var spawn_point: Node2D = $SpawnPoint
 @onready var go_downstairs: InteractionArea = $"Go Downstairs"
 @onready var box: InteractionArea = $Box
+@onready var toilet: InteractionArea = $Toilet
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 const PLAYER = preload("res://Scenes/player.tscn")
 
 func _ready() -> void:
 	go_downstairs.interact = Callable(self, "_go_downstairs")
 	box.interact = Callable(self, "_interact_box")
+	toilet.interact = Callable(self, "_interact_toilet")
 	
 	if GlobalVariables.is_new_game or GlobalVariables.saved_scene_name != get_tree().current_scene.scene_file_path:
 		# Create the player when new game
@@ -22,3 +25,9 @@ func _go_downstairs():
 
 func _interact_box():
 	Dialog.launch_dialog(Dialog.BABY_BOX)
+
+func _interact_toilet():
+	GlobalVariables.is_in_cinematic = true
+	animation_player.play("toilet")
+	await animation_player.animation_finished
+	GlobalVariables.is_in_cinematic = false
